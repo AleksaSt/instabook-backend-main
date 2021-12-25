0d989f93ba4d71c8f42ca182fc756733da7c739c
AleksaSt <alexastojcevic@yahoo.com>
2021-12-25 14:49:17 +0100

    auth controllers updated

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
     public function register(Request $request) {
       $user = new User();
+      $user->name = $request->name;
       $user->email = $request->email;
       $user->password = bcrypt($request->password);
       $user->save();
